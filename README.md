f39667d77415f73988de8489e5dbc6b530b1dbb5
Sem Onyalo <oonyalo@gmail.com>
2018-02-26 17:40:32 -0500

    initial code commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## IoT Sandbox - Server
---
+++ payload.go
@@ -0,0 +1,9 @@
+package main
+
+type Payload struct {
+	// The client type of the source's message
+	SrcType string
+
+	// The payload message
+	Message string
+}
